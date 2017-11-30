519faa1507fb120e99353be351457683a4c80f12
Zoran Simic <zsimic@netflix.com>
2017-11-30 03:03:24 -0800

    Test bash upgrade script as well

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -1,19 +1,41 @@
 import os
 import shutil
+import subprocess
 import tempfile
+
+import pytest
 
 import setupmeta
 import conftest
 
 SETUPMETA = os.path.join(conftest.PROJECT, 'setupmeta.py')
 GH = 'https://raw.githubusercontent.com/zsimic/setupmeta/master/setupmeta.py'
+LOCAL_URL = 'file://%s' % os.path.join(conftest.PROJECT, 'setupmeta.py')
 
 
-def run_self_upgrade(args):
+@pytest.fixture(params=['setupmeta', 'bash'])
+def mode(request):
+    """ Yield one test per scenario """
+    yield request.param
+
+
+def run_self_upgrade(mode, args):
     with conftest.capture_output() as (out, err):
         exit_msg = ''
         try:
-            setupmeta.self_upgrade(args)
+            if mode == 'bash':
+                os.environ['URL'] = LOCAL_URL
+                script = os.path.join(conftest.PROJECT, 'get-setupmeta')
+                p = subprocess.Popen(
+                    [script] + args,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                )
+                output, error = p.communicate()
+                return setupmeta.to_str(output) + setupmeta.to_str(error)
+            else:
+                setupmeta.__url__ = LOCAL_URL
+                setupmeta.self_upgrade(args)
         except SystemExit as e:
             # self_upgrade() calls sys.exit()
             exit_msg = "%s" % e
@@ -22,54 +44,55 @@ def run_self_upgrade(args):
         return out + err + exit_msg
 
 
-def run_setupmeta(args, *expected):
-    output = run_self_upgrade(args)
+def do_run(mode, args, *expected):
+    output = run_self_upgrade(mode, args)
     for msg in expected:
         assert msg in output
     return output
 
 
-def test_upgrade():
+def test_upgrade(mode):
     """ Test that upgrade functionality works """
     tmpdir = tempfile.mkdtemp()
-    local_url = 'file://%s' % os.path.join(conftest.PROJECT, 'setupmeta.py')
-    setupmeta.__url__ = local_url
 
     try:
-        # Sanity check --help
-        run_setupmeta(['--help'], "Install/upgrade")
+        if mode != 'bash':
+            # Sanity check --help
+            do_run(mode, ['--help'], "Install/upgrade")
 
-        # Bogus target
-        run_setupmeta(['foo/bar'], "not a valid directory")
+            # Bogus target
+            do_run(mode, ['foo/bar'], "not a valid directory")
 
-        # Bogus invalid url
-        run_setupmeta([tmpdir, '-ufoo'], "Could not fetch foo")
+            # Bogus invalid url
+            do_run(mode, [tmpdir, '-ufoo'], "Could not fetch foo")
 
-        # Bogus existing url
-        url = 'file://%s' % os.path.join(conftest.PROJECT, 'classifiers.txt')
-        run_setupmeta([tmpdir, '-u', url], "Invalid url", "please check")
+            # Bogus existing url
+            url = os.path.join(conftest.PROJECT, 'classifiers.txt')
+            url = 'file://%s' % url
+            do_run(mode, [tmpdir, '-u', url], "Invalid url", "please check")
 
-        # dryrun
-        run_setupmeta([tmpdir, '-n'], "Would seed", "--dryrun")
+            # dryrun
+            do_run(mode, [tmpdir, '-n'], "Would seed", "--dryrun")
 
         # 1st run: seed setupmeta
-        run_setupmeta([tmpdir], "Seeded ")
+        do_run(mode, [tmpdir], "Seeded ")
 
         # 2nd run: we're up to date now
-        run_setupmeta([tmpdir], "Already up to date")
+        do_run(mode, [tmpdir], "Already up to date")
 
         # simulate file changed
         with open(os.path.join(tmpdir, 'setupmeta.py'), 'a') as fh:
             fh.write('added one line')
 
-        # dryrun
-        run_setupmeta([tmpdir, '-n'], "Would upgrade", "--dryrun")
+        if mode != 'bash':
+            # dryrun
+            do_run(mode, [tmpdir, '-n'], "Would upgrade", "--dryrun")
 
         # 3rd run: we're not up to date anymore
-        run_setupmeta([tmpdir], "Upgraded ")
+        do_run(mode, [tmpdir], "Upgraded ")
 
         # 4th run: up to data again now
-        run_setupmeta([tmpdir], "Already up to date")
+        do_run(mode, [tmpdir], "Already up to date")
 
     finally:
         shutil.rmtree(tmpdir)
