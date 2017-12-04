9322c8b82622fccd392d91e7deb3d5a6c3ff8d5e
Zoran Simic <zsimic@netflix.com>
2017-12-04 01:25:52 -0800

    Bumped version

--- setupmeta/__init__.py
+++ setupmeta/__init__.py
@@ -2,7 +2,7 @@
 See https://github.com/zsimic/setupmeta
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __license__ = 'MIT'
 __url__ = "https://github.com/zsimic/setupmeta"
 __download_url__ = 'archive/v{version}.tar.gz'
