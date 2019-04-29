60448fcb9480ad2c15c6c796283242d25ca7e99c
Zoran Simic <zsimic@netflix.com>
2019-04-29 13:20:43 -0700

    Simplified wheel test (pip is less verbose now)

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -1,38 +1,22 @@
 import os
-import re
-
-from mock import patch
 
 import setupmeta
 
 
-RE_META = re.compile(r""".*['"](\S+/METADATA)['"].*""")
-
-
-def get_metadata_line(output):
-    for line in output.split("\n"):
-        m = RE_META.match(line)
-        if m:
-            return m.group(1)
-
-
-@patch.dict(os.environ, {"SETUPMETA_DEBUG": "1"})
 def test_wheel(sample_project):
     # Fake existence of a build folder, that should be in .gitignore (but isn't)
-    # Presence of a new file should not mark version as dirty
+    # Presence of a new file there should not mark version as dirty
     build_folder = os.path.join(sample_project, "build")
     os.mkdir(build_folder)
     with open(os.path.join(build_folder, "report.txt"), "w") as fh:
         fh.write("This is some build report\n")
 
-    output = setupmeta.run_program("pip", "-vvv", "wheel", "--only-binary", ":all:", "-w", "dist", ".", capture=True)
-    meta = get_metadata_line(output)
-    assert meta == "sample-0.1.0.dist-info/METADATA"
+    setupmeta.run_program("pip", "wheel", "--only-binary", ":all:", "-w", "dist", ".")
+    assert os.path.isfile(os.path.join(sample_project, "dist", "sample-0.1.0-py3-none-any.whl"))
 
     # Now let's modify one of the files
     with open(os.path.join(sample_project, "sample.py"), "w") as fh:
         fh.write("print('hello')\n")
 
-    output = setupmeta.run_program("pip", "-vvv", "wheel", "--only-binary", ":all:", "-w", "dist", ".", capture=True)
-    meta = get_metadata_line(output)
-    assert meta == "sample-0.1.0.dirty.dist-info/METADATA"
+    setupmeta.run_program("pip", "wheel", "--only-binary", ":all:", "-w", "dist", ".")
+    assert os.path.isfile(os.path.join(sample_project, "dist", "sample-0.1.0.dirty-py3-none-any.whl"))
