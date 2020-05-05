5d78e6dd16d3b8a7f0f70a2bc9d7105002c8d6d5
Zoran Simic <zsimic@netflix.com>
2020-05-04 20:41:18 -0700

    Run new via-cfg test only on py3.7+

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -2,6 +2,8 @@
 Verify that ../examples/*/setup.py behave as expected
 """
 
+import sys
+
 import pytest
 
 import setupmeta
@@ -11,13 +13,18 @@ from . import scenarios
 
 @pytest.fixture(params=scenarios.scenario_paths())
 def scenario_folder(request):
-    """ Yield one test per scenario """
+    """Yield one test per scenario"""
     with setupmeta.temp_resource():
         yield request.param
 
 
 def test_scenario(scenario_folder):
-    """ Check that 'scenario' yields expected explain output """
+    """Check that 'scenario' yields expected explain output"""
+    py = ".".join(str(s) for s in sys.version_info[:2])
+    if py < "3.7" and "via-cfg" in scenario_folder:
+        # For some reason, older pythons don't all seem to handle setup.cfg well... maybe min version of setuptools needed?
+        pytest.skip("via-cfg scenario useful only in 3.7+")
+
     scenario = scenarios.Scenario(scenario_folder)
     expected = scenario.expected_contents()
     output = scenario.replay()
